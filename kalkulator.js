1994f49d2b6697df836546cbb0be49907f75841b
Achmad Tirto Sudiro <118708640+Achmadts@users.noreply.github.com>
2022-11-29 15:26:33 +0700

    Create kalkulator.js

---
+++ kalkulator.js
@@ -0,0 +1,31 @@
+const readline = require("readline");
+
+const calcInterface = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+const validOperators = [
+	"*",
+	"-",
+	"+",
+	"/",
+];
+
+calcInterface.question("Masukkan bilangan ke-1: ", (firstInput) => {
+	const firstNumber = Number(firstInput);
+    console.log(firstNumber);
+	if(isNaN(firstNumber)) {
+		console.log("Masukkan Angka!");
+		return calcInterface.close();
+	}
+  
+  console.log('==================================');
+
+	console.log (" Masukkan " + " + " + " - " + " * " + " or " + " / ")
+	calcInterface.question("Masukkan operator: ", (operatorInput) => {
+		if(!validOperators.includes(operatorInput)) {
+			console.log("Operator salah");
+			return calcInterface.close();
+		}
+	console.log('==================================');
